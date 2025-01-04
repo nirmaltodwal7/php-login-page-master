cf8e34593b5fcb2723df35647e31ed3fa57215c1
Nirmal Todwal <162917515+nirmaltodwal7@users.noreply.github.com>
2025-01-05 01:20:49 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','user_db');
+
+?>
+
+<!-- CREATE TABLE `user_db`.`user_form` (`id` INT(255) NOT NULL , `name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `ph-no` INT(255) NOT NULL ) ENGINE = InnoDB;  -->
